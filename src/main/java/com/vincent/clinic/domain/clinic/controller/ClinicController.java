c9799e810d9b5b886661d9faf89735e8b4d32005
이동철 <choise154@gmail.com>
2023-09-24 17:38:50 +0900

    :lipstick: 부트스트랩으로 요소들 추가(pages: 홈, 작성, 수정)

--- src/main/java/com/vincent/clinic/domain/clinic/controller/ClinicController.java
+++ src/main/java/com/vincent/clinic/domain/clinic/controller/ClinicController.java
@@ -1,7 +1,9 @@
 package com.vincent.clinic.domain.clinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,19 @@ public class ClinicController {
     @GetMapping("")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/clinic/add")
+    public String add(Model model) {
+        return "add";
+    }
+
+    @GetMapping("/clinic/{no}/edit")
+    public String edit(
+            Model model,
+            @PathVariable Long no
+    ) {
+        model.addAttribute("no", no);
+        return "add";
     }
 }
