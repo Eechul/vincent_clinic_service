aba63af53663c976092b2070721ba0b3215e37a9
이동철 <choise154@gmail.com>
2023-10-04 15:06:13 +0900

    :sparkles: springdoc-openapi 적용(springdoc이 swagger-ui를 지원)

--- src/main/java/com/vincent/clinic/domain/clinic/controller/ClinicController.java
+++ src/main/java/com/vincent/clinic/domain/clinic/controller/ClinicController.java
@@ -1,5 +1,7 @@
 package com.vincent.clinic.domain.clinic.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Tag(name = "Clinic", description = "진료에 관한 API")
 @RequestMapping("/clinic")
 public class ClinicController {
 
+    @Operation(summary = "진료일지 메인 페이지(목록 페이지)")
     @GetMapping("")
     public String index(Model model) {
         model.addAttribute("name", "home");
         return "index";
     }
 
+    @Operation(summary = "진료일지 상세 페이지")
     @GetMapping("/{no}")
     public String clinicContent(
             @PathVariable Long no,
@@ -25,12 +30,14 @@ public class ClinicController {
         return "content";
     }
 
+    @Operation(summary = "진료일지 추가 페이지")
     @GetMapping("/add")
     public String add(Model model) {
         model.addAttribute("name", "add");
         return "add";
     }
 
+    @Operation(summary = "진료일지 수정 페이지")
     @GetMapping("/{no}/edit")
     public String edit(Model model, @PathVariable Long no) {
         model.addAttribute("name", "edit");
