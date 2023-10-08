79a43ba5cdb0dc84fa05a269553e3fc2554786ee
이동철 <choise154@gmail.com>
2023-10-08 19:53:53 +0900

    :sparkles: 통합 테스트 전용 추상클래스 추가

---
+++ src/test/java/com/vincent/clinic/GenerateIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.vincent.clinic;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class GenerateIntegrationTest {
+}
