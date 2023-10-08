3079c26c07286a6fc492e08f5ecb67ccb3e0d3c9
이동철 <choise154@gmail.com>
2023-10-09 01:16:03 +0900

    :sparkles: 전역 interceptor 추가

--- src/main/java/com/vincent/clinic/VincentClinicConfig.java
+++ src/main/java/com/vincent/clinic/VincentClinicConfig.java
@@ -1,16 +1,31 @@
 package com.vincent.clinic;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.vincent.clinic.domain.department.service.DepartmentService;
+import com.vincent.clinic.global.interceptor.VincentCinicInterceptor;
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing
+@RequiredArgsConstructor
 public class VincentClinicConfig implements WebMvcConfigurer {
+
+    private final DepartmentService departmentService;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new VincentCinicInterceptor(departmentService))
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
---
+++ src/main/java/com/vincent/clinic/global/interceptor/VincentCinicInterceptor.java
@@ -0,0 +1,27 @@
+package com.vincent.clinic.global.interceptor;
+
+import com.vincent.clinic.domain.department.dto.DepartmentDto;
+import com.vincent.clinic.domain.department.service.DepartmentService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.List;
+
+public class VincentCinicInterceptor implements HandlerInterceptor {
+
+    private DepartmentService departmentService;
+
+    public VincentCinicInterceptor(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        List<DepartmentDto> clinicDepartments = departmentService.findAll();
+        request.setAttribute("navDepartments", clinicDepartments);
+        return true;
+    }
+
+
+}
