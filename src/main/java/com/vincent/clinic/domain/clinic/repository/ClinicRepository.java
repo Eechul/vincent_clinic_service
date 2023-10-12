b422bbaf801e24982fa18480e6d3856271956ccc
이동철 <choise154@gmail.com>
2023-10-12 17:30:56 +0900

    :memo: 진료 페이징 설계

--- src/main/java/com/vincent/clinic/domain/clinic/repository/ClinicRepository.java
+++ src/main/java/com/vincent/clinic/domain/clinic/repository/ClinicRepository.java
@@ -1,10 +1,14 @@
 package com.vincent.clinic.domain.clinic.repository;
 
 import com.vincent.clinic.domain.clinic.entity.Clinic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClinicRepository extends JpaRepository<Clinic, Long> {
 
+    @Override
+    Page<Clinic> findAll(Pageable pageable);
 }
--- src/main/java/com/vincent/clinic/domain/clinic/service/ClinicService.java
+++ src/main/java/com/vincent/clinic/domain/clinic/service/ClinicService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ClinicService {
 
+//    List<ClinicDto>
     void accept(ClinicAcceptServiceRequest serviceRequest);
     void modify();
 
