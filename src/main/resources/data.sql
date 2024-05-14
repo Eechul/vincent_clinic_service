465f718b99f75a48a720e20cf519f5093264ca4d
이동철 <choise154@gmail.com>
2024-05-14 22:56:27 +0900

    profile active 'test' 환경 재구성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO vc_department(description, name, path, created_at) VALUES('내과 입니다.', '내과', 'internal', NOW());
+INSERT INTO vc_department(description, name, path, created_at) VALUES('정형외과 입니다.', '정형외과', 'orthopedics', NOW());
+INSERT INTO vc_department(description, name, path, created_at) VALUES('치과 입니다.', '치과', 'dentist', NOW());
+INSERT INTO vc_department(description, name, path, created_at) VALUES('산부인과 입니다.', '산부인과', 'obstetrics', NOW());
