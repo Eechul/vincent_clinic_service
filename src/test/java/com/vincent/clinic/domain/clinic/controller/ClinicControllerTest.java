81b55e96d4e02eeeb5759a59ec4be5ce69ea11aa
이동철 <choise154@gmail.com>
2024-05-23 23:53:39 +0900

    진료일지 컨트롤러 테스트(WebMvcTest) 시도

---
+++ src/test/java/com/vincent/clinic/domain/clinic/controller/ClinicControllerTest.java
@@ -0,0 +1,39 @@
+package com.vincent.clinic.domain.clinic.controller;
+
+import com.vincent.clinic.domain.clinic.service.ClinicService;
+import com.vincent.clinic.domain.department.service.DepartmentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = { ClinicController.class })
+public class ClinicControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private DepartmentService departmentService;
+    @MockBean
+    private ClinicService clinicService;
+
+
+    @BeforeEach
+    void setUp() { }
+
+    @DisplayName("진료일지 루트 접근 시, /clinic/internal 로 리다이렉트 한다.")
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/clinic"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/clinic/internal"));
+    }
+
+}
